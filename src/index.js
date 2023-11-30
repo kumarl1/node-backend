2d7b8d8a5f2b753608582b9477134798b757a65c
kumarl1 <lalit.sharma385@gmail.com>
2023-11-30 14:25:30 +0530

    Mongo db connection completed

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+// require('dotenv').config({path:'./env'});
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./.env'
+})
+
+connectDB();
+
+// import express from 'express';
+// const  app = express();
+// (async () =>{
+//     try {
+//         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     }catch(error){
+//         console.log('Error', error)
+//     }
+// })
