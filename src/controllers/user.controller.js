3d4564786dfdd065df4a897552627e21d77c01d4
kumarl1 <lalit.sharma385@gmail.com>
2023-12-20 17:35:42 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -156,7 +156,9 @@ const logoutUser = asyncHandler(async (req,res) =>{
 })
 
 const refreshAccesToken = asyncHandler( async (req,res) =>{
-    const incomingRefreshToken =req.cookie.refreshToken || req.body.refreshToken;
+    
+    const incomingRefreshToken =req?.cookie?.refreshToken || req.body.refreshToken;
+    
     if(!incomingRefreshToken){
         throw new ApiError(401, "Unauthorized request");
     }
